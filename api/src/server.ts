4d886d10ce981f54ddc55d41bb053c16addeafc7
Gabriel Fragoso <gabriel.fragoso@lab510.br>
2023-03-19 16:12:47 -0300

    feat: added cors

--- api/src/server.ts
+++ api/src/server.ts
@@ -7,9 +7,14 @@ import { router } from "./routes";
 
 const app = express();
 app.use(express.json());
-app.use(cors());
 
 app.use(router);
+
+const corsOptions = {
+  origin: "https://ilumeo-beta.vercel.app",
+};
+
+app.use(cors(corsOptions));
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof Error) {

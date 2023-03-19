3e1e3eb355fb211081c25776c5f6327f509f50e3
Gabriel Fragoso <gabriel.fragoso@lab510.br>
2023-03-19 17:04:31 -0300

    teste2

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -11,7 +11,6 @@ import { UpdatePointController } from "./controllers/point/UpdatePointController
 import { GetPointByUserController } from "./controllers/point/GetPointByUserController";
 
 const router = Router();
-app.use(cors());
 
 // -- Rotas Users -- //
 router.get("/users", cors(), async (req, res) => {
